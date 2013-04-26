ce73a183f1946a31c12ba8f30d960dbb012babbb
Barrett <musikal.fusion@gmail.com>
2013-04-25 19:32:54 -0700

    Update grouping.py

--- grouping.py
+++ grouping.py
@@ -1,1 +1,21 @@
-G
+from collections import defaultdict
+
+def grouper(iterable, key=lambda x: x):
+  ''' Group an iterable by some key function.
+  
+  By default this groups based on value.
+  ie: all key and value pairs will be identical.
+  
+  Required arguments:
+  iterable -- an iterable object
+  
+  Keyword arguments:
+  key -- function to be called on each element, determines what is grouped by
+  
+  returns a defaultdict of key's and value lists containing the groups
+  '''
+  result = defaultdict(list)
+  for it in iterable:
+    result[key(it)].append(it)
+    
+  return result
