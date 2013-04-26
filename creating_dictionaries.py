1b2c87d527265ef92768ff7e940d962fb82910bb
Barrett <musikal.fusion@gmail.com>
2013-04-25 21:50:35 -0700

    Update creating_dictionaries.py

--- creating_dictionaries.py
+++ creating_dictionaries.py
@@ -1,4 +1,4 @@
-# from 2 lists
+# from 2 iterables
 keylist = ['k1', 'k2', 'k3']
 vallist = ['v1', 'v2', 'v3']
 d = dict(zip(keylist, vallist))
@@ -6,3 +6,6 @@ d = dict(zip(keylist, vallist))
 # from iterable of tuples, note this is with a list but works with any iterable of tuples
 kvs = [('k1', 'v1'), ('k2', 'v2'), ('k3', 'v3')]
 d = dict(kvs)
+
+# explicit creation
+d = {'k1':'v1', 'k2':'v2', 'k3','v3'}
