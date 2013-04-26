81acc912bc11cca98100c1aa9a337130ae45359f
Barrett <musikal.fusion@gmail.com>
2013-04-25 22:01:37 -0700

    Update creating_dictionaries.py

--- creating_dictionaries.py
+++ creating_dictionaries.py
@@ -4,6 +4,7 @@ vallist = ['v1', 'v2', 'v3']
 d = dict(zip(keylist, vallist))
 
 # from iterable of tuples, note this is with a list but works with any iterable of tuples
+# also note they don't have to be tuples a list of lists works here as well
 kvs = [('k1', 'v1'), ('k2', 'v2'), ('k3', 'v3')]
 d = dict(kvs)
 
