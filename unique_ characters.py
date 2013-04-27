eae08c4f244345d8a7b3ef30cf810a5e8a638ee5
Barrett <musikal.fusion@gmail.com>
2013-04-27 12:38:26 -0700

    Update and rename unique_ characters.py to unique_ elements.py

--- unique_ characters.py
+++
@@ -1,6 +0,0 @@
-s = input('give me a string')
-
-if len(s) == len(set(s)):
-  print('all characters unique')
-else:
-  print('not all characters were unique')
---
+++ unique_ elements.py
@@ -0,0 +1,5 @@
+# where s is an iterable
+if len(s) == len(set(s)):
+  print('all elemetns unique')
+else:
+  print('not all elements were unique')
